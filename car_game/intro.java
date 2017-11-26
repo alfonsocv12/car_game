3847a9a9962cdd5d1cf6dc3daaecfa8c12bc1c1d
alfonso villalobos <judo_alfonso@hotmail.com>
2017-11-26 11:37:00 -0800

    re acomodo

--- car_game/intro.java
+++ car_game/intro.java
@@ -9,6 +9,9 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import car_game.index;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
